6b0544b63e257c89d76b97fc40d485c5358fb32a
JagjitBhosale <150025654+JagjitBhosale@users.noreply.github.com>
2025-08-20 02:25:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import FullscreenCodeViewer from './components/FullscreenCodeViewer';
 import { io } from 'socket.io-client';
 
 const App = () => {
-  const socket = useMemo(() => io("http://localhost:5000"), []);
+  const socket = useMemo(() => io("https://codesender-backend-production.up.railway.app/"), []);
   
   const [currentRoom, setCurrentRoom] = useState('');
   const [roomCode, setRoomCode] = useState('');
